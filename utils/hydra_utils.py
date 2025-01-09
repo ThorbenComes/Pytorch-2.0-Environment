6753e43d84009175b0b071ef3bab7e6931cece31
N <thorbencomes@protonmail.com>
2025-01-09 16:15:35 +0100

    added util to enhance config readability during debugging

--- utils/hydra_utils.py
+++ utils/hydra_utils.py
@@ -1,1 +1,23 @@
 """Class offering utilities for the hydra configuration manager"""
+
+
+def prettyPrint(dict_in, message=None):
+    """prints a nested dictionary in a more readable way"""
+    if message is not None:
+        print(message)
+    print(tabAdder(dict_in))
+
+
+def tabAdder(dict_in, tabs=0):
+    out = []
+    for key in dict_in:
+        if isinstance(dict_in[key], dict):
+            out.append(key + ":\n" + tabAdder(dict_in[key], tabs + 1))
+        else:
+            out.append(key + ": " + dict_in[key].__str__())
+
+    tab_string = ["\t" for _ in range(tabs)]
+    repeated_string = "\n" + "".join(tab_string)
+
+    return repeated_string[1:] + repeated_string.join(out)
+
