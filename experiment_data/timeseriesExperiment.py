439e44e741094ccfc856f9126b00a154280b30e8
N <thorbencomes@protonmail.com>
2025-01-17 15:52:15 +0100

    updated experiment

--- experiment_data/timeseriesExperiment.py
+++ experiment_data/timeseriesExperiment.py
@@ -1,36 +1,39 @@
+import torch.utils.data
+
 from models_layers import modelManager
-from experiment_data import optimizerManager
+from learning import optimizerManager
+from learning import learningManager
 import wandb
 from omegaconf import OmegaConf
-from credentials import wandb_api_key
+import flatten_dict
+import utils.logs.logWriter
+from experiment_data import dataLoader
 
 
 class Experiment:
-    def __init__(self, configuration, data_dict):
+    def __init__(self):
+        self.configuration = wandb.config
+        print(self.configuration)
+        # self.data_dict = dataLoader.load(configuration)
 
-        self.data_dict = data_dict
+    def _init(self):
 
-    def _wandb_init(self, wandb_login = False):
-        ## Convert Omega Config to Wandb Config
-        config_dict = OmegaConf.to_container(self.cfg)
-        try:
-            exp_name = self.model_cfg.wandb.exp_name + self.model_cfg.parameters.learn.name
-        except:
-            exp_name = self.model_cfg.wandb.exp_name + self.learn_cfg.name
-        if self.model_cfg.wandb.log:
-            mode = "online"
-        else:
-            mode = "disabled"
-        ## Initializing wandb object and sweep object
-        # if self.model_cfg.wandb.log:
-        # wandb.login(key=wandb_api_key, relogin=True)
-        # self.wandb_config = wandb.config
-        if wandb_login:
-            wandb.login(key=wandb_api_key, relogin=True)
-        wandb_run = wandb.init(config=config_dict, project=self.model_cfg.wandb.project_name, name=exp_name,
-                               mode=mode)  # wandb object has a set of configs associated with it as well
-        return wandb_run
-
-    def init_train_eval_model(self):
         wandb_run = self._wandb_init()
+        config = OmegaConf.create(flatten_dict.unflatten_dict(wandb.config))
+        self.cfg = config
+
+        self.model = modelManager.model(config)
+        self.optimizer = optimizerManager.optimizer(config, self.model.parameters())
+        self.learning_algorithm = learningManager.algorithm(config)
+        self.train_dataset = torch.utils.data.TensorDataset(self.data_dict["train_obs"], self.data_dict["train_targets"])
+        self.eval_dataset = torch.utils.data.TensorDataset(self.data_dict["test_obs"], self.data_dict["test_targets"])
+        self.logWriter = utils.logs.logWriter.logWriter(config, wandb_run)
+
+    def train(self):
+        print("starting training")
         pass
+
+    def eval(self):
+        print("starting evaluation")
+        pass
+
